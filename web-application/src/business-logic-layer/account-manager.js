8638435fe24f7bab68bc1489c37dabf2d023b50f
Jake <jacob.carlquist@hotmail.com>
2019-12-25 15:05:11 +0100

    First commit

---
+++ web-application/src/business-logic-layer/account-manager.js
@@ -0,0 +1,32 @@
+const accountRepository = require('../data-access-layer/account-repository')
+const accountValidator = require('./account-validator')
+
+exports.getAllAccounts = function(callback){
+	accountRepository.getAllAccounts(callback)
+}
+
+exports.createAccount = function(account, callback){
+	
+	// Validate the account.
+	console.log("account manager")
+	console.log("account: ", account)
+	const errors = accountValidator.getErrorsNewAccount(account)
+	
+	if(0 < errors.length){
+		callback(errors, null)
+		return
+	}
+	
+	accountRepository.createAccount(account, callback)
+	
+}
+
+exports.getAccountByUsername = function(username, callback){
+	accountRepository.getAccountByUsername(username, callback)
+}
+
+exports.loginRequest = function(account, callback){
+	console.log("account user name in BL: ", account.username)
+	accountRepository.getAccountByUsername(account.username, callback)
+	
+}
